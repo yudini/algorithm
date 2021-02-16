dc13d21f10cc6a8e8c23665b78bdd0da4fe16980
yudini <oyj0922@naver.com>
2021-02-16 21:41:48 +0900

    2579

---
+++ 2579.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int n;
+    int stairs[301];
+    int dp[301];
+
+    cin>>n;
+    for(int i=1;i<=n;i++)
+        cin>>stairs[i];
+        
+    dp[0]=0; //점화식 쓰기 편리.    
+    dp[1]=stairs[1];
+    dp[2]=stairs[1]+stairs[2];
+    
+    for(int i=3;i<=n;i++){
+        dp[i]=max(stairs[i-1]+dp[i-3],dp[i-2])+stairs[i];
+    }
+
+    cout<<dp[n];
+    //for(int i=1;i<=n;i++)
+     //   cout<<dp[i]<<"\n";
+
+}
