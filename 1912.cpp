5ac27e48d80e5cc47165fe25636760fb09aedb74
yudini <oyj0922@naver.com>
2021-02-16 21:39:58 +0900

    1912 수정

--- 1912.cpp
+++ 1912.cpp
@@ -9,18 +9,18 @@ int main(){
     cout.tie(NULL);
     int N;
     int array[100001];
-    int d[100001];
+    int dp[100001];
     cin>>N;
     for(int i=0;i<N;i++){
         cin>>array[i];
     }
     int result=-1000;
-    d[0]=array[0];
+    dp[0]=array[0];
     for(int i=1;i<N;i++){
-        d[i]=max(d[i-1]+array[i],array[i]);
-        result=max(result,d[i]);
+        dp[i]=max(dp[i-1]+array[i],array[i]);
+        result=max(result,dp[i]);
     }
-    result=max(result,d[0]);
+    result=max(result,dp[0]);
 
     cout<<result;
 
