1d78f3b2a5159256af33f7db409e6dd227fe6725
yudini <oyj0922@naver.com>
2021-04-10 23:19:16 +0900

    2753

---
+++ BOJ/2753.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int year;
+
+    scanf("%d",&year);
+    if(year%4==0){
+       if(year%100!=0 || year%400==0)
+           printf("1");
+        else
+            printf("0");
+    }else 
+        printf("0");
+
+    return 0;
+}
