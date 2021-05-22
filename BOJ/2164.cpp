ae10e894b01609dc91057c593eb8c4b6c15806dc
yudini <oyj0922@naver.com>
2021-05-22 23:13:33 +0900

    2164 카드2

---
+++ BOJ/2164.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int N;
+    int x=0;
+    queue <int> q;
+    cin>>N;
+
+
+/*
+q.pop();
+q.pop(q.front());
+q.pop();
+*/
+    for(int i=1;i<=N;i++){
+        q.push(i);
+    }
+
+    while(q.size()!=1){
+
+        /*
+        q.pop();
+        q.pop(q.front());
+        q.pop();
+        */
+        if(x%2==0){
+            q.pop();
+        }
+        else{
+            int k= q.front();
+            q.push(k);  
+            q.pop();
+        }
+        x++;
+    }
+
+    cout<<q.front();
+}
